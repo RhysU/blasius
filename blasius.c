b0fda20a7fbef66e2c0f66919eed8a8da2c6fef6
Rhys Ulerich <rhys.ulerich@gmail.com>
2013-10-21 09:05:09 -0500

    Permit running backwards-in-time
    Specifying a negative etaf parameter now permits running the ODE
    backward in time.  This is handy to get a couple of Blasius solution
    points prior to eta = 0 so that interpolation routines that set
    natural end conditions can be fooled with data prior to the region
    of interest we genuinely want.

--- blasius.c
+++ blasius.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <gsl/gsl_errno.h>
 #include <gsl/gsl_machine.h>
+#include <gsl/gsl_math.h>
 #include <gsl/gsl_minmax.h>
 #include <gsl/gsl_odeiv2.h>
 
@@ -14,7 +15,7 @@ enum { ndim = 3 };
 
 // Evaluate the Blasius function RHS
 static inline int
-func (double eta, const double f[ndim], double df[ndim], void *params)
+forward (double eta, const double f[ndim], double df[ndim], void *params)
 {
     (void) eta;
     (void) params;
@@ -22,6 +23,15 @@ func (double eta, const double f[ndim], double df[ndim], void *params)
     df[1] = f[2];
     df[2] = - f[0] * f[2] / 2;
     return GSL_SUCCESS;
+}
+
+// Use \tilde{\eta} = -\eta to reverse evolution for increasing \tilde{\eta}
+static inline int
+backward (double eta, const double f[ndim], double df[ndim], void *params)
+{
+    const int status = forward(-eta, f, df, params);
+    for (int i = 0; i < ndim; ++i) df[i] = -df[i];
+    return status;
 }
 
 // Use GSL ODEIV2 to advance the solution outputting at regular intervals
@@ -33,9 +43,11 @@ main (int argc, const char *argv[])
     const double deleta = (argc > 2) ? atof(argv[2]) :  0.2;
     const double tol    = (argc > 3) ? atof(argv[3]) : GSL_DBL_EPSILON;
 
-    // Define the problem, tolerance, and initial step size
+    // Define the problem, tolerance, and initial step size including
+    // performing forward or backwards integration from eta=0 initial condition
+    const gsl_odeiv2_system sys = {etaf > 0 ? forward : backward, 0L, ndim, 0L};
+
     // Do /not/ blindly trust the tolerance as Blasius is notoriously difficult
-    const gsl_odeiv2_system sys = {func, NULL, ndim, NULL};
     gsl_odeiv2_driver* const d = gsl_odeiv2_driver_alloc_yp_new (
             &sys, gsl_odeiv2_step_rk8pd, tol*10, tol, 0.0);
 
@@ -46,15 +58,17 @@ main (int argc, const char *argv[])
 
     // Integrate until either final time achieved or error encountered
     int err = 0;
-    printf("%22s  %22s  %22s  %22s\n", "eta", "f", "fp", "fpp");
-    for (int niter = 0; eta < etaf; ++niter) { // Odd, but avoids drift
-        err = gsl_odeiv2_driver_apply (d, &eta, GSL_MIN(etaf, niter*deleta), f);
+    printf("%23s  %23s  %23s  %23s\n", "eta", "f", "fp", "fpp");
+    const double aetaf = fabs(etaf);
+    for (int niter = 0; eta < aetaf; ++niter) { // Odd, but avoids drift
+        err = gsl_odeiv2_driver_apply(d, &eta, GSL_MIN(aetaf, niter*deleta), f);
         if (err) {
             fprintf(stderr, "At %g encountered error %d: %s\n",
-                    eta, err, gsl_strerror(err));
+                    GSL_SIGN(etaf)*eta, err, gsl_strerror(err));
             break;
         }
-        printf("%22.16e  %22.16e  %22.16e  %22.16e\n", eta, f[0], f[1], f[2]);
+        printf("%23.16e  %23.16e  %23.16e  %23.16e\n",
+               GSL_SIGN(etaf)*eta, f[0], f[1], f[2]);
     }
 
     gsl_odeiv2_driver_free (d);
